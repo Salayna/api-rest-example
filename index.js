9ecdd673ce3cfb1d268767ffd7fbcfade8db5a0d
Salayna <salaynadoukoure@gmail.com>
2021-03-03 13:03:21 +0100

    changing names

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.json())
+app.use(express.json())
 
 let cars = [];
 
 let car1 = {
     id: 1,
     name: "Tesla Model S",
-    proprio: "Mr Iervese",
+    proprio: "Mr Ren",
     date: 2017,
 }
 
 let car2 = {
     id: 2,
     name: "Lamborghini Urus",
-    proprio: "Mr Hagot",
+    proprio: "Mr Pierre",
     date: 2019,
 }
 
 let car3 = {
     id: 3,
     name: "Ferrari Roma",
-    proprio: "Mr Georges",
+    proprio: "Mr Henry",
     date: 2016,
 }
 
@@ -47,14 +46,11 @@ app.get('/api/cars/:id', (req, res)=>{
 
 app.post('/api/cars', (req, res)=> {
  const body = req.body;
- if(!req.body.id ||!req.body.name || !req.body.proprio || !req.body.date) {
+ if(!body.id ||!body.name || !body.proprio || !body.date) {
      res.status(400).send("Bad Request");
      return
  }
- if((typeof req.body.name !== 'string' || req.body.name instanceof String === false) ||(typeof req.body.proprio !== 'string' || req.body.proprio instanceof String === false) || (typeof req.body.date !== 'number' || req.body.date instanceof Number === false) || (typeof req.body.id !== 'number' || req.body.id instanceof Number === false)){
-     res.status(400).send("Bad Request");
-     return
- }
+
  cars.push(body);
  res.status(201).send({
      message: "Entity added to the db",
@@ -75,7 +71,6 @@ app.patch('/api/car/:id', (req, res)=>{
     cars[id-1].proprio = proprio;
     res.status(200).send(cars[id-1]);
 });
-
 
 app.delete('/api/car/:id', (req, res)=>{
     const id = req.params.id;
